1f20ba2bf6c1fd3f87ab5acbdafbb29d58fd802e
CarlNathan <c.n.udren@gmail.com>
2015-12-20 10:41:27 -0500

    Initial Commit

---
+++ Toyota's are Cars too!/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyota's are Cars too!
+//
+//  Created by Carl Udren on 12/20/15.
+//  Copyright © 2015 Carl Udren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
